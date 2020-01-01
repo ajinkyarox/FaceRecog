0a6e30cdb707bf2de0272d71433dc0fd0e4cb404
ajinkyarox <rairikar.ajinkya@gmail.com>
2020-01-01 17:13:11 +0530

    GET call added

--- Django/fc_django/facerecog/urls.py
+++ Django/fc_django/facerecog/urls.py
@@ -1,9 +1,10 @@
 # pages/urls.py
 from django.urls import path
 
-from .views import homePageView
+from .views import homePageView,getEmpDetails
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', homePageView, name='home'),
+    path('empdetails', getEmpDetails, name='getEmpDetails')
 ]
 
--- Django/fc_django/facerecog/views.py
+++ Django/fc_django/facerecog/views.py
@@ -1,7 +1,10 @@
 # pages/views.py
 from django.http import HttpResponse
+from facerecog.models import EmpDetails
 
 
 def homePageView(request):
     return HttpResponse('Hello from Django!')
 
+def getEmpDetails(request):
+    return HttpResponse(EmpDetails.objects.all())
