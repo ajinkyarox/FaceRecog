f0e9d26660b6d83367fe0499f5a7e9d7b5609dca
ajinkyarox <rairikar.ajinkya@gmail.com>
2020-01-20 17:50:27 +0530

    Added Training model functionality

--- Django/fc_django/facerecog/views.py
+++ Django/fc_django/facerecog/views.py
@@ -8,6 +8,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import QueryDict
 from os import path
 import json
+import cv2
+import numpy as np
+import re
+from re import search
 
 def homePageView(request):
     return HttpResponse('Hello from Django!')
@@ -125,6 +129,13 @@ def savePhoto(request):
                     with open(filepath, "wb") as fh:
                         fh.write(base64.decodebytes(arr[1].encode('utf-8')))
                         response = {'status': 'Success', 'responseObject': None}
+                        print("Success- -1")
+                        faces=labels_for_training_data(cwdname)
+                        print("Interim")
+                        face_recognizer = train_classifier(faces, body_data['id'])
+                        print("Success-0")
+                        face_recognizer.write(cwdname+os.path.sep+'trainingData.yml')
+                        print("Success-1")
                 else:
                     response = {'status': 'Failure', 'responseObject': None}
 
@@ -148,3 +159,64 @@ def markAttendance(request):
 def getAttendance(request):
     data = list(Attendance.objects.values())
     return JsonResponse(data, safe=False)
+
+
+def faceDetection(test_img):
+    gray_img=cv2.cvtColor(test_img,cv2.COLOR_BGR2GRAY)#convert color image to grayscale
+    print("H1")
+    cwdname = os.path.abspath(os.getcwd() + os.path.sep + "haarcascade_frontalface_default.xml")
+    print("H2")
+    face_haar_cascade=cv2.CascadeClassifier(cwdname)#Load haar classifier
+    faces=face_haar_cascade.detectMultiScale(gray_img,scaleFactor=1.32,minNeighbors=5)#detectMultiScale returns rectangles
+
+    print("H3")
+    return faces,gray_img
+
+#Given a directory below function returns part of gray_img which is face alongwith its label/ID
+def labels_for_training_data(directory):
+    faces=[]
+
+    for path,subdirnames,filenames in os.walk(directory):
+        for filename in filenames:
+            print(filename)
+            if filename.startswith("."):
+                print("Skipping system file")#Skipping files that startwith .
+                continue
+            #elif search("jpg",filename) and search("png",filename) and search("JPG",filename) and search("PNG",filename):
+            print("Training...")
+            id=os.path.basename(path)#fetching subdirectory names
+            img_path=os.path.join(path,filename)#fetching image path
+            print("img_path:",img_path)
+            print("id:",id)
+            test_img=cv2.imread(img_path)#loading each image one by one
+            print("T1")
+            if test_img is None:
+                print("Image not loaded properly")
+                continue
+            print("T2")
+            faces_rect,gray_img=faceDetection(test_img)#Calling faceDetection function to return faces detected in particular image
+            if len(faces_rect)!=1:
+                continue #Since we are assuming only single person images are being fed to classifier
+            (x,y,w,h)=faces_rect[0]
+            roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from grayscale image
+            faces.append(roi_gray)
+    print(len(faces))
+    return faces
+
+
+#Below function trains haar classifier and takes faces,faceID returned by previous function as its arguments
+def train_classifier(faces,faceID):
+    faceID=np.full(len(faces),faceID)
+    face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+    face_recognizer.train(faces,faceID)
+    return face_recognizer
+
+#Below function draws bounding boxes around detected face in image
+def draw_rect(test_img,face):
+    (x,y,w,h)=face
+    cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=5)
+
+#Below function writes name of person for detected label
+def put_text(test_img,text,x,y):
+    cv2.putText(test_img,text,(x,y),cv2.FONT_HERSHEY_DUPLEX,2,(255,0,0),4)
+
